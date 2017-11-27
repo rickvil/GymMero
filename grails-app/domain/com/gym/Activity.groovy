c2798ff98c7f8c56a9673d27046d74b2532994f3
Ricardo Daniel Vilte <dvilte@theinit.com>
2017-11-26 22:06:45 -0300

    update domain

--- grails-app/domain/com/gym/Activity.groovy
+++ grails-app/domain/com/gym/Activity.groovy
@@ -2,6 +2,9 @@ package com.gym
 
 class Activity {
 
+    String title
+
     static constraints = {
+        title(nullable: false, size: 0..50)
     }
 }
--- grails-app/domain/com/gym/TypeActivity.groovy
+++ grails-app/domain/com/gym/TypeActivity.groovy
@@ -2,6 +2,9 @@ package com.gym
 
 class TypeActivity {
 
+    String title
+
     static constraints = {
+        title(nullable: false, size: 0..50)
     }
 }
