c7f3657179801d2fdd751a4714fb855b8f2eacfb
Ricardo Daniel Vilte <dvilte@theinit.com>
2017-11-28 01:55:57 -0300

    add domain contracted pack

---
+++ grails-app/domain/com/gym/ContractedPack.groovy
@@ -0,0 +1,21 @@
+package com.gym
+
+class ContractedPack {
+
+    User user
+    Activity activity
+    TypeActivity typeActivity
+    String barCode
+    Date contractedDate
+    Integer credit
+    boolean free
+
+    static constraints = {
+        user(nullable: false)
+        activity(nullable: false)
+        typeActivity(nullable: false)
+        barCode(nullable: true)
+        credit(nullable: true, inList: [0, 8, 12, 20])
+        free(nullable: false)
+    }
+}
